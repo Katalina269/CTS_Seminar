7b6e4c9c495185d3c1bf9cbf28f447b937990e6e
Katalina269 <katalina.ceban99@gmail.com>
2021-02-27 14:24:39 +0200

    + animal and zookeeper class

---
+++ CTS-Seminar1/src/clase/Zookeeper.java
@@ -0,0 +1,22 @@
+package clase;
+
+public class Zookeeper {
+	private String ingrijitor;
+
+	public String getIngrijitor() {
+		return ingrijitor;
+	}
+
+	public void setIngrijitor(String ingrijitor) {
+		this.ingrijitor = ingrijitor;
+	}
+
+	public Zookeeper(String ingrijitor) {
+		super();
+		this.ingrijitor = ingrijitor;
+	}
+	
+    public void feed(Animal a) {
+    	System.out.println("Ingrijitorul" +this.ingrijitor +", hraneste animalul => " + a.getNume());
+    }
+}
