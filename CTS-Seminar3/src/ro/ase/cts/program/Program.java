6ea9bcfdc46a019a5480c13ed7aff415b722c3d9
Katalina269 <katalina.ceban99@gmail.com>
2021-03-13 14:41:49 +0200

    Principiu DRY, modificare functie citire

---
+++ CTS-Seminar3/src/ro/ase/cts/program/Program.java
@@ -0,0 +1,34 @@
+package ro.ase.cts.program;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+
+import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.readers.ReaderAngajat;
+import ro.ase.cts.readers.ReaderAplicanti;
+
+
+public class Program {
+	
+	public static List<Aplicant> citesteAplicanti(ReaderAplicanti reader) throws FileNotFoundException{
+		List<Aplicant> listaAplicanti=reader.readAplicanti();
+		return listaAplicanti;
+	}
+	
+
+	public static void main(String[] args) {
+		List<Aplicant> listaAplicanti;
+		try {
+			listaAplicanti = citesteAplicanti(new ReaderAngajat("angajati.txt"));
+			for(Aplicant aplicant:listaAplicanti) {
+				System.out.println(aplicant.toString());
+				aplicant.afisareSumaFinantata();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
